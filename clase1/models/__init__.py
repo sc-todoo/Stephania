4e1d1a626a0a799d2095d5813db99451fa036411
Stephania <sc@todoo.co>
2020-02-26 23:55:47 +0000

    Nuevo

--- clase1/models/__init__.py
+++ clase1/models/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: utf-8 -*-
-
-from . import Clase
+from . import clase
---
+++ clase1/models/clase.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class clase1(models.Model):
+    _name = 'Clase.clase1'
+    _description = 'Esta clase ayuda al aprendizaje'
+
+    name= fields.Char()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    active = fields.Boolean(string='Active', default=True)
+    jugador = fields.Integer()
+    valoracion  = fields.Float(compute="_value_pc", store=True)
+    descripcion = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
